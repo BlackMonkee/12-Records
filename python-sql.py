bb507a5092b9a1e20270786f879012d729f439e6
BlackMonkee <procoderman189@gmail.com>
2024-08-19 09:20:24 +0530

    python-sql.py
    ch14

--- python-sql.py
+++ python-sql.py
@@ -4,5 +4,10 @@ sql.connect(user='root',host='localhost',passwd='0009',database='yuvan12')
 mycon=sql.connect(user='root',host='localhost',passwd='0009',database='yuvan12')
 if mycon.is_connected():
     print("GAE")
+    mcur=mycon.cursor()
+    mcur.execute("select * from product")
+    data=mcur.fetchall()
+    for i in data:
+        print(i)
 else:
     print("NOGAE")
